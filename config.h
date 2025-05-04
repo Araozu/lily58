1a9d6a1bfe27bf7c14a29e30d9b09561ebd2105e
Fernando Araoz <fernando@araozu.dev>
2025-05-04 14:27:22 -0500

    feat: improve layout, add cirque 35mm

--- config.h
+++ config.h
@@ -22,10 +22,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
-#define MASTER_LEFT
-// #define MASTER_RIGHT
+// #define MASTER_LEFT
+#define MASTER_RIGHT
 // #define EE_HANDS
 
 #define QUICK_TAP_TERM 0
 #define TAPPING_TERM 150
 
+// #define RP2040_FLASH_GD25Q64CS
--- rules.mk
+++ rules.mk
@@ -1,6 +1,6 @@
 LTO_ENABLE = yes            # Link Time Optimization enabled
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no        # Mouse keys
+MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = no        # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
@@ -9,13 +9,20 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
-OLED_ENABLE = yes           # OLED display
+OLED_ENABLE = no            # OLED display
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
+CIRQUE_PINNACLE_DIAMETER_MM = 35
+CIRQUE_PINNACLE_POSITION_MODE = CIRQUE_PINNACLE_RELATIVE_MODE
+CIRQUE_PINNACLE_TAP_ENABLE = yes
+CIRQUE_PINNACLE_SECONDARY_TAP_ENABLE = yes
+CIRQUE_PINNACLE_ATTENUATION = EXTREG__TRACK_ADCCONFIG__ADC_ATTENUATE_2X
 
 # If you want to change the display of OLED, you need to change here
-SRC +=  ./lib/rgb_state_reader.c \
-        ./lib/layer_state_reader.c \
-        ./lib/logo_reader.c \
-        ./lib/keylogger.c \
-        # ./lib/mode_icon_reader.c \
-        # ./lib/host_led_state_reader.c \
-        # ./lib/timelogger.c \
+# SRC +=  ./lib/rgb_state_reader.c \
+#         ./lib/layer_state_reader.c \
+#         ./lib/logo_reader.c \
+#         ./lib/keylogger.c \
+#         # ./lib/mode_icon_reader.c \
+#         # ./lib/host_led_state_reader.c \
+#         # ./lib/timelogger.c \
